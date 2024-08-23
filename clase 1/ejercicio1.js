d4580d4490c51adbdf6cc646e2167f880577985f
pannzzz <maria.jimenez544@soy.sena.edu.co>
2024-08-22 21:47:21 -0500

    se cambio el resultado de puntaje de la evaluacion para que saliera exelente

--- clase 1/ejercicio1.js
+++ clase 1/ejercicio1.js
@@ -1,5 +1,5 @@
 /* ejercicio de puntaje Crea una variable de puntaje y usa condiciones para mostrar excelente si el puntaje es mayor o igual a 90 bueno si el puntaje está entre 70 y 89 necesita mejorar si el puntaje es menor de 70 */
-let puntaje = 85;
+let puntaje = 100;
 
 if (puntaje >= 90) {
     puntaje = "Excelente";
